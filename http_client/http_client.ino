cbe10adb1bf6194db78f3c3b46ef9bc90d003edd
Parthav46 <parthav46@gmail.com>
2020-02-06 16:58:49 +0530

    added node HTTPClient

---
+++ http_client/http_client.ino
@@ -0,0 +1,28 @@
+#include<ESP8266WiFi.h>
+#include<ESP8266HTTPClient.h>
+
+#define wifiSSID "sudhanshusinh"
+#define wifiPSWD "sudhanshu"
+
+HTTPClient http;
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(wifiSSID, wifiPSWD); 
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  http.begin("http://worldclockapi.com/api/json/utc/now");
+  int httpCode = http.GET();
+  if(httpCode == HTTP_CODE_OK) {
+    Serial.println(http.getString());
+  }
+}
+
+void loop() {
+}
